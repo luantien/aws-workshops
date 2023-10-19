d097d649584f16828425cc63dc19b0e478e51dc3
luantien <luantien411@gmail.com>
2023-10-19 15:00:52 +0700

    Init CDK Stack for Integration Workshop

---
+++ workshop-2/test/integration-workshop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Workshop2 from '../lib/workshop-2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/workshop-2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Workshop2.Workshop2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
