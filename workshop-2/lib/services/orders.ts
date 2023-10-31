92f83c14d26a79d8d4e7d550a34e0816d0368bb7
luantien <luantien411@gmail.com>
2023-10-31 12:58:17 +0700

    Add EventBridge Pipe

--- workshop-2/lib/services/orders.ts
+++ workshop-2/lib/services/orders.ts
@@ -2,7 +2,12 @@ import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 import * as agw from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as event_targets from 'aws-cdk-lib/aws-events-targets';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { UserPoolClient } from 'aws-cdk-lib/aws-cognito';
+import { CfnPipe } from 'aws-cdk-lib/aws-pipes';
 // Common Resource Templates
 import { LambdaFunctionProps, LambdaHandler } from '../templates/lambda-handler';
 import { DynamoDb } from '../templates/dynamodb';
@@ -58,6 +63,7 @@ export class OrdersService extends Construct {
                     projectionType: DynamoDb.ProjectionType.ALL,
                 }
             ],
+            stream: DynamoDb.StreamViewType.NEW_IMAGE,
         });
 
         const lambdaOptions = {
@@ -101,12 +107,13 @@ export class OrdersService extends Construct {
         }
 
         
-        this.provistionOrderResources(lambdaOptions, authorizer);
+        this.provisionOrderResources(lambdaOptions, authorizer);
 
+        this.provisionDownStreamPipeline();
         this.generateCfnOutput();
     }
 
-    protected provistionOrderResources(
+    protected provisionOrderResources(
         lambdaOptions: LambdaFunctionProps, 
         authorizer?: agw.CognitoUserPoolsAuthorizer
     ) {
@@ -157,8 +164,128 @@ export class OrdersService extends Construct {
         );
 
         this.dynamodb.table.grantReadData(handlers.getOrderDetail.function);
-        this.dynamodb.table.grantWriteData(handlers.createOrder.function);
-        this.dynamodb.table.grantReadData(handlers.createOrder.function);
+        this.dynamodb.table.grantReadWriteData(handlers.createOrder.function);
+    }
+
+    protected provisionDownStreamPipeline() {
+        const pipeDLQ = new sqs.Queue(this, 'PipeDLQ', {
+            queueName: `${this.owner}PipeDLQ`,
+        });
+
+        const orderEventBus = new events.EventBus(this, 'OrderEventBus', {
+            eventBusName: `${this.owner}OrderEventBus`,
+        });
+
+        const eventPipeRole = new iam.Role(this, 'EventBridgePipeRole', {
+            roleName: `${this.owner}EventBridgePipeRole`,
+            description: 'Role to allow Pipes to send events to EventBridge',
+            assumedBy: new iam.ServicePrincipal('pipes.amazonaws.com'),
+        });
+        this.dynamodb.table.grantStreamRead(eventPipeRole);
+        orderEventBus.grantPutEventsTo(eventPipeRole);
+        pipeDLQ.grantSendMessages(eventPipeRole);
+
+        const eventPipe = new CfnPipe(this, 'EventBridgePipe', {
+            name: `${this.owner}EventBridgePipe`,
+            description: 'Pipe to connect Orders DynamoDB Table stream to EventBridge Event Bus',
+            roleArn: eventPipeRole.roleArn,
+            source: this.dynamodb.table.tableStreamArn!,
+            sourceParameters: {
+                dynamoDbStreamParameters: {
+                    startingPosition: 'LATEST',
+                    batchSize: 1, // Set the batch size to 1
+                    deadLetterConfig: {
+                        arn: pipeDLQ.queueArn,
+                    },
+                },
+                filterCriteria: {
+                    filters: [
+                        {
+                            pattern: '{ "eventName": ["INSERT", "MODIFY"] }'
+                        },
+                    ],
+                }
+            },
+            target: orderEventBus.eventBusArn,
+            targetParameters: {
+                eventBridgeEventBusParameters: {
+                    detailType: 'OrderChanged',
+                    source: 'aws:dynamodb',
+                },
+                // DynamoDB Stream Record
+                // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_streams_Record.html
+                inputTemplate: `{
+                    "meta": {
+                        "correlationId": <$.eventID>,
+                        "changeType": "OrderChangedEvent"
+                    },
+                    "content": <$.dynamodb>
+                }`
+            },
+        });
+        
+        const orderCreatedQueue = new sqs.Queue(this, 'OrderCreatedQueue', {
+            queueName: `${this.owner}OrderCreatedQueue.fifo`,
+            fifo: true,
+            deduplicationScope: sqs.DeduplicationScope.MESSAGE_GROUP,
+            contentBasedDeduplication: true,
+            deadLetterQueue: {
+                maxReceiveCount: 3,
+                queue: new sqs.Queue(this, 'OrderCreatedDLQ', {
+                    queueName: `${this.owner}OrderCreatedDLQ.fifo`,
+                    fifo: true,
+                }),
+            },
+        });
+
+        const orderConfirmedQueue = new sqs.Queue(this, 'OrderConfirmedQueue', {
+            queueName: `${this.owner}OrderConfirmedQueue.fifo`,
+            fifo: true,
+            deduplicationScope: sqs.DeduplicationScope.MESSAGE_GROUP,
+        });
+
+        const queuePolicy = new sqs.CfnQueuePolicy(this, 'OrderCreatedQueuePolicy', {
+            policyDocument: new iam.PolicyDocument({
+                statements: [
+                    new iam.PolicyStatement({
+                        effect: iam.Effect.ALLOW,
+                        principals: [new iam.ServicePrincipal('events.amazonaws.com')],
+                        actions: ['sqs:SendMessage'],
+                        resources: [orderCreatedQueue.queueArn],
+                    }),
+                ],
+            }),
+            queues: [orderCreatedQueue.queueUrl],
+        });
+
+        const orderCreatedEventRule = new events.Rule(this, 'OrderCreatedRule', {
+            eventBus: orderEventBus,
+            ruleName: `${this.owner}OrderCreatedRule`,
+            description: 'Rule to capture order created events',
+            targets: [
+                new event_targets.SqsQueue(orderCreatedQueue, {
+                    messageGroupId: 'OrderCreated',
+                    retryAttempts: 3, // Set the retry policy to 3 attempts
+                    maxEventAge: cdk.Duration.seconds(300),  // Set the maxEventAge retry policy to 5 minutes
+                }),
+            ],
+            // EventBride Event Structure
+            // https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-events-structure.html
+            eventPattern: {
+                detailType: ['OrderChanged'],
+                detail: {
+                    meta: {
+                        changeType: ['OrderChangedEvent'],
+                    },
+                    content: {
+                        "NewImage": {
+                            "EntityType": { "S": ["order"]},
+                            "Status": {"S": ["CREATED"]},
+                        }
+                    }
+                },
+            },
+        });
     }
 
     public generateCfnOutput() {
