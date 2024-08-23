1905d8d7f623512a4e637fb1e194093360506f17
Tien Nguyen <luantien411@gmail.com>
2024-08-23 14:00:44 +0700

    Fix: MainStack Id

--- workshop-2/bin/integration-workshop.app.ts
+++ workshop-2/bin/integration-workshop.app.ts
@@ -8,7 +8,7 @@ const app = new cdk.App();
 const username = process.env.AWS_USERNAME || 'anonymous';
 
 
-new MainStack(app, 'iws-main-stack', {
+new MainStack(app, 'MainStack', {
     stackName: `${username.toUpperCase()}-IntegrationWorkshop`,
     description: 'AWS Integration Workshop with EventBridge, SQS, SNS, and StepFunctions',
 });
